b072d8ac2d7c8901744b0948d660f0c91ca8809a
thanhlt-1007 <le.tan.thanh@sun-asterisk.com>
2025-02-22 19:29:37 +0700

    Add get_name_with_age.py

---
+++ get_name_with_age.py
@@ -0,0 +1,13 @@
+def get_name_with_age(name: str, age: int):
+    name_with_age = name + " is this old " + str(age)
+    return name_with_age
+
+"""
+More motivation
+
+Check this function, it already has type hints:
+
+Because the editor knows the types of the variables, you don't only get completion, you also get error checks:
+
+Now you know that you have to fix it, convert age to a string with str(age):
+"""
